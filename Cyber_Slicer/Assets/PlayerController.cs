b0625940ded0fa41a4dabb90a549e6be5b840cd5
Kyle G <42879674+geekguy100@users.noreply.github.com>
2018-09-07 17:51:14 -0500

    Added PlayerControl script

---
+++ Cyber_Slicer/Assets/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float movementSpeed;
+
+    private void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Vector2 vel = new Vector2(h * movementSpeed, v * movementSpeed);
+    }
+}
