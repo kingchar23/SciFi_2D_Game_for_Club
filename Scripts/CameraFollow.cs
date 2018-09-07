76ef16b9d0258a3e771ccc40d249e4d563ab37ce
Kyle G <42879674+geekguy100@users.noreply.github.com>
2018-09-07 18:46:06 -0500

    PlayerController and CameraFollow scripts

---
+++ Scripts/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private Vector3 offset;
+    [SerializeField]
+    private float smoothTime;
+    [SerializeField]
+    private float minYPos;
+    [SerializeField]
+    private float maxYPos;
+
+    private Vector3 vel;
+
+    private void Start()
+    {
+        if (!target)
+            Debug.LogWarning("Camera has no target!");
+    }
+
+    private void Update()
+    {
+        if (!target) return;
+        Vector3 pos = Vector3.SmoothDamp(transform.position, target.position + offset, ref vel, smoothTime);
+        pos.y = Mathf.Clamp(pos.y, minYPos, maxYPos);
+        transform.position = pos;
+    }
+}
---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float movementSpeed;
+    [SerializeField]
+    private float minYPos;
+    [SerializeField]
+    private float maxYPos;
+
+    private void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 vel = new Vector2(h * movementSpeed, v * movementSpeed);
+
+        Vector3 pos = transform.position + vel * Time.deltaTime;
+        pos.y = Mathf.Clamp(pos.y, minYPos, maxYPos);
+        transform.position = pos;
+    }
+}
